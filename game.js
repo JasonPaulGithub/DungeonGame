f337e228d6e7393938e4037ddb52d1657a196723
gapspaceengineers <gapspace@engineer.com>
2016-05-14 01:03:51 +0200

    Upload---3
    ---Done---
    Boilerplate.
    Pathfinding (FInally!!).
    
    ---Next---
    Update the response code.
    Impelment the level (Json).

--- game.js
+++ game.js
@@ -1,27 +1,62 @@
-var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example', { preload: preload, create: create });
+// Easy Star JS setup::
+var easystar = new EasyStar.js();
+var timeStep = 400; // pathway computation time interval in milliseconds
+
+var level = [[0,0,1,0,0],
+    [1,0,1,0,1],
+    [0,0,1,0,0],
+    [0,0,1,1,0],
+    [0,0,0,0,0]];
+
+easystar.setGrid(level);
+easystar.setAcceptableTiles([0]);
+easystar.enableDiagonals();
+easystar.enableCornerCutting();
+//
+
+
+
+
+var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example', { preload: preload, create: create, update:update });
 
 var loadingLabel;
+var response = 'Check console for pathfinding';
 
 function preload() {
 
-    //  Specify a unique key and a URL path
-    //  The key must be unique between all images.
     game.load.image('imageKey', 'assets/img/ghost.png');
-
-}
-
-function create() {
     game.add.plugin(Phaser.Plugin.Debug);
     game.add.plugin(Phaser.Plugin.Inspector);
 
-    game.add.image(95, 300, 'imageKey');
-    loadingLabel = game.add.text(200, 150, 'The game is loading...', {font: '30px Courier', fill: '#fff'});
 }
 
 
-//paste as little as possible at a time, and test it then move on...
+function create() {
+    game.add.image(95, 300, 'imageKey');
 
+    easystar.findPath(0, 0, 4, 0, function( path ) {
+        if (path === null) {
+            console.log("The path to the destination point was not found.");
+            response = "DORMANT";
+        }
 
+        else {
 
+            for (var i = 0; i < path.length; i++)
+            {
+                console.log("P: " + i + ", X: " + path[i].x + ", Y: " + path[i].y);
+            }
 
+            response = 'RUNNING';
+        }
 
+    });
+
+    easystar.calculate();
+
+    loadingLabel = game.add.text(200, 150, 'PATHFINDING: ' + response, {font: '22px Courier', fill: '#fff'});
+}
+
+    function update(){
+        // We need to place the response code here so it can be updated. Though it's working in the console is fine for now.
+    }
