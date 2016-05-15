7ccbe8480ec4b632a38ec55cd383b4de5cef6382
gapspaceengineers <gapspace@engineer.com>
2016-05-15 20:50:05 +0200

    Upload---3
    ---Done---
    Boilerplate.
    Pathfinding (FInally!!).
    
    ---Next---
    Update the response code.
    Impelment the level (Json).

--- game.js
+++ game.js
@@ -1,6 +1,5 @@
 // Easy Star JS setup::
 var easystar = new EasyStar.js();
-var timeStep = 400; // pathway computation time interval in milliseconds
 
 var level = [[0,0,1,0,0],
     [1,0,1,0,1],
@@ -12,27 +11,71 @@ easystar.setGrid(level);
 easystar.setAcceptableTiles([0]);
 easystar.enableDiagonals();
 easystar.enableCornerCutting();
-//
-
-
-
 
 var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example', { preload: preload, create: create, update:update });
+
+function preload() {
+
+    game.load.image('tiles','src/sprites/tiles.png');
+    game.load.tilemap('mapData', 'src/map/testMap.json', null, Phaser.Tilemap.TILED_JSON);
+    game.load.json('map', 'src/map/testMap.json', null, Phaser.Tilemap.TILED_JSON);
+    game.load.image('ghost', 'src/sprites/ghost.png');
+
+    //Load the plugin
+    game.add.plugin(Phaser.Plugin.Debug);
+    game.add.plugin(Phaser.Plugin.Inspector);
+
+    var data = {
+        "0": "1",
+        "1": "2",
+        "2": "3",
+        "3": "4"
+    };
+    var arr = [];
+    for (var prop in data) {
+        arr.push(data[prop]);
+    }
+    console.log(arr);
+}
 
 var loadingLabel;
 var response = 'Check console for pathfinding';
 
-function preload() {
-
-    game.load.image('imageKey', 'assets/img/ghost.png');
-    game.add.plugin(Phaser.Plugin.Debug);
-    game.add.plugin(Phaser.Plugin.Inspector);
-
-}
-
+var mapData;
+var background_layer;
+var floor_layer;
+var floor_decor;
+var wall_layer;
+var wall_decor;
+var roof_layer;
 
 function create() {
-    game.add.image(95, 300, 'imageKey');
+
+    game.add.image(95, 300, 'ghost');
+
+    game.physics.startSystem(Phaser.Physics.P2JS);
+
+    mapData = game.add.tilemap('mapData');
+    mapData.addTilesetImage('tiles');
+
+    background_layer = mapData.createLayer('background');
+    background_layer.resizeWorld();
+    background_layer.debug = false;
+    floor_layer = mapData.createLayer('floor');
+    floor_layer.resizeWorld();
+    floor_layer.debug = false;
+    floor_decor = mapData.createLayer('floor_decor');
+    floor_decor.resizeWorld();
+    floor_decor.debug = false;
+    wall_layer = mapData.createLayer('wall');
+    wall_layer.resizeWorld();
+    wall_layer.debug = true;
+    wall_decor = mapData.createLayer('wall_decor');
+    wall_decor.resizeWorld();
+    wall_decor.debug = false;
+    roof_layer = mapData.createLayer('roof');
+    roof_layer.resizeWorld();
+    roof_layer.debug = false;
 
     easystar.findPath(0, 0, 4, 0, function( path ) {
         if (path === null) {
@@ -58,5 +101,5 @@ function create() {
 }
 
     function update(){
-        // We need to place the response code here so it can be updated. Though it's working in the console is fine for now.
+
     }
