49c3dee81a67fd59574c6e5da38dff4413b002d1
gapspaceengineers <gapspace@engineer.com>
2016-06-03 18:51:13 +0200

    Upload --- V-4.4

--- game.js
+++ game.js
@@ -154,6 +154,30 @@ function create() {
     /* use enableDiagonals on small enemies, make their collision radius smaller, and
     /  allow them to slow or halt the player
     /  allow for large enemies to have trouble moving around the map
+
+     /'choose your door.....'
+    /combat ideas: cleave. Rage. stun. charged attack. charge attack.
+    /defence spells. summons. pets. AOE damage system. projectiles.
+    /druid class (change into an animal?)
+    /smoke. invisibility. mixing toxins for cloud-based AOE attacks.
+    /attack combos! parry(combo breakers) enemies block distinct patterns
+    /beserker rage enemy. being flanked. chase mechanics.
+    /poisions, elemental damage, slow, and skill trees (perhaps only the mage can have this for now)
+    /stealth kills
+
+    / choose combo template: high, low, flanker, attack, block, parry (parry will relfect a percent of physical damage)
+    / enemies counter patterns, like consecutive attacks low to high to low, but not high, block, low
+    / opening attacks sets the base damage at %50, so an opening attack of 10 means the most damage made can be 5, crits hit up to 10, multipliers not included
+    / weapons dont crit but will pass on damage onto flanking enemies, or STORE for an ulti attack.
+    / more combos and abilties can be unlocked via the skill tree
+    / near-death combos / abilities can be unlocked
+
+    / mages can take down groups of weak eneimes and need XP to become better,
+    / the theif takes down large enemies and can escape well against groups
+    / fighters good all round but weak vs undead, clerics are tanks and speclaise against undead
+    / it's all about the shop! play for money, items, & spells that mess with game mechanics.
+
+    / with all this in mind start off with the basics, obvs...
     */
 
     setInterval(function(){
@@ -226,6 +250,9 @@ function create() {
 
 function orcAttack(body){
     if (body){
+        //this will fire even if the orc hits a wall.
+        //ona side note, enemies might fight amogst themselves
+        //even a 'rage' style spell might work.
         stopPathFinder = true;
         orc.animations.play('attack');
     }
