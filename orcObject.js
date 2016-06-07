10b285809b2fb7fb4e304764d27fafa080a4301e
gapspaceengineers <gapspace@engineer.com>
2016-06-08 00:29:36 +0200

    Upload --- V-5

--- orcObject.js
+++ orcObject.js
@@ -17,6 +17,7 @@ orcObject = function (game){
 
 orcObject.getValue= function(){//<<<< note no .prototype. here<<<
     //we need to find a way to get a response out of this object so we can experiment with the easyStarObject.js file.
+    //perhaps we could rename it to the 'pathfinderService' or some thing gay like that...
     return 'Implement me!';
     //it might be worth just taking the code from the TANKS example to see if it works.
     //we could also just use a global variable in game.js as a response for now.
