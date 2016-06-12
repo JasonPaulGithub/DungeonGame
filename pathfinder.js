ebf4c97573031e8ec38caf587cf657c55ab12ec5
gapspaceengineers <gapspace@engineer.com>
2016-06-12 23:36:26 +0200

    Upload --- V-8

--- pathfinder.js
+++ pathfinder.js
@@ -1,5 +1,18 @@
 pathfinder = function (){
 
+    var easystar = new EasyStar.js();
+    easystar.setGrid(level);
+    easystar.setAcceptableTiles([0]);
 
+    easystar.findPath(1, 1, 2, 2, function (path) {
 
+        for (var i = 0; i < path.length; i++)
+        {
+            console.log("X: " + path[i].x + " Y: " + path[i].y);
+
+        }
+
+    });
+    easystar.calculate();
+    delete this;
 }
