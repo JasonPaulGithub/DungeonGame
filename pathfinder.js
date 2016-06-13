56920418284080aa0a708582ae5420a6090c962b
gapspaceengineers <gapspace@engineer.com>
2016-06-13 02:14:19 +0200

    Upload --- V-8.1

--- pathfinder.js
+++ pathfinder.js
@@ -7,9 +7,57 @@ pathfinder = function (obj_x, obj_y, player_x, player_y){
 
     easystar.findPath(obj_x, obj_y, player_x, player_y, function (path) {
 
-        for (var i = 0; i < path.length; i++)
+        if (path.length > 8 || path === null)
         {
-            console.log("X: " + path[i].x + " Y: " + path[i].y);
+
+        } else {
+
+            for (var i = 0; i < path.length; i++)
+            {
+                if (path[1].x < obj_x && path[1].y < obj_y) {
+                    debug1 = "NW";
+                }
+                else if (path[1].x == obj_x && path[1].y < obj_y) {
+                    debug1 = "N";
+                }
+                else if (path[1].x > obj_x && path[1].y < obj_y) {
+                    debug1 = "NE";
+                }
+                else if (path[1].x < obj_x && path[1].y == obj_y) {
+                    debug1 = "W";
+                }
+                else if (path[1].x > obj_x && path[1].y == obj_y) {
+                    debug1 = "E";
+                }
+                else if (path[1].x > obj_x && path[1].y > obj_y) {
+                    debug1 = "SE";
+                }
+                else if (path[1].x == obj_x && path[1].y > obj_y) {
+                    debug1 = "S";
+                }
+                else if (path[1].x < obj_x && path[1].y > obj_y) {
+                    debug1 = "SW";
+                }
+                else {
+                    debug1 = "STOP";
+                }
+
+                moveEnemyObj();
+                //Here the same problem occurs twice:
+
+                /*
+
+                good news is the direcitons are being passed on
+                to debug1, but sadly this is a variable, we need some thing
+                that can focus on the object. The same thing foes for the
+                move enemy object variable.
+
+                To solve this I must investigate using:
+                Messing with functions, prototype or the RETURN keyword.
+                GETters and SETters. Anything.
+
+                 */
+            }
         }
 
     });
