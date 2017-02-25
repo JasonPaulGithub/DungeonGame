46b248b6c846dbe67f0258cae043cd1896cd8e7d
gapspaceengineers <gapspace@engineer.com>
2017-02-25 21:32:56 +0100

    Our Magic Entity Value created! Game variables are now stored in a variable and adapts a bit of OOP. Little Monster Debugger!

--- newGame.js
+++ newGame.js
@@ -1,4 +1,4 @@
-var game = new Phaser.Game(600, 600, Phaser.AUTO, 'phaser-example', {
+var game = new Phaser.Game(1200, 600, Phaser.AUTO, 'phaser-example', {
     preload: preload,
     create: create,
     update: update,
@@ -17,7 +17,11 @@ function preload() {
 var mapData;
 var upKey;
 
+// Designed to hold a collection of interchangable game objects (i.e player, enemies,etc).
 var entityCollection =[['hello number 1.1','hello number 1.2'],['Hello number 2']];
+
+//Desinged to collect screen text
+var textCollection=[];
 
 function create()
 {
@@ -26,21 +30,31 @@ function create()
     runMapData();
 
     //Entity Factory
-    function Entity(xLoc,yLoc){
+    function Entity(id,xLoc,yLoc){
+        this.id   = id;
         this.xLoc = xLoc;
         this.yLoc = yLoc;
     }
-
     //Construct Entity
-    var entity1 = new Entity('Magic Entity Value 1',10);
+    var entity1 = new Entity('Player_Entity',10,10);
     entityCollection.push(entity1)
 
-    game.add.text(100, 75, 'Press up to test:', { font: "15px Arial", fill: "#19de65" });
+    //Screen Text
+    textCollection.push('GAME STATS\n');
+    textCollection.push(',,";-;",,, -( Entity: ' + entity1.id +' : X: ' + entityCollection[2]+' Y:'+entityCollection[2].yLoc+' !)');
+    var text = game.add.text(10, 10, textCollection);
+    text.font = 'Arial Black';
+    text.fontSize = 15;
+    text.fontWeight = 'bold';
+    text.fill = '#ee0000';
+    text.setShadow(0, 0, 'rgba(200, 200, 200, 0.5)', 0);
 
+/*
     game.add.text(100, 150, 'Entity 1: '+ entityCollection[0][0], { font: "15px Arial", fill: "#19de65" });
     game.add.text(100, 175, 'Entity 2: '+ entityCollection[1], { font: "15px Arial", fill: "#19de65" });
     game.add.text(100, 200, 'Entity 3: '+ entityCollection[2].xLoc.toString(), { font: "15px Arial", fill: "#19de65" });
-
+    game.add.text(100, 300, 'Press up to test:', { font: "15px Arial", fill: "#19de65" });
+*/
 }
 
 function update()
@@ -51,6 +65,8 @@ function render()
 {
     if (upKey.isDown == true)
     {
-        game.add.text(100, 125, 'UP HAS BEEN PRESSED', { font: "15px Arial", fill: "#19de65" });
+        game.add.text(100, 325, 'UP HAS BEEN PRESSED', { font: "15px Arial", fill: "#19de65" });
+        game.add.text(entityCollection)
+
     }
 }
