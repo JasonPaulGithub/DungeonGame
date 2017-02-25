e8887b9a5959b37bbd7c66fd48f4affd1d674187
gapspaceengineers <gapspace@engineer.com>
2017-02-25 21:42:46 +0100

    Our Magic Entity Value created! Game variables are now stored in a variable and adapts a bit of OOP. Little Monster Debugger!

--- newGame.js
+++ newGame.js
@@ -41,36 +41,29 @@ function create()
 
     //Screen Text
     textCollection.push('GAME STATS\n');
-    textCollection.push(',,";-;",,, -( Entity: ' + entity1.id +' : X: ' + entityCollection[2]+' Y:'+entityCollection[2].yLoc+' !)');
+    textCollection.push(',,";-;",,, -( Entity: ' + entity1.id +' : X: ' + entityCollection[2].xLoc+' Y:'+entityCollection[2].yLoc+' !)');
     var text = game.add.text(10, 10, textCollection);
     text.font = 'Arial Black';
     text.fontSize = 15;
     text.fontWeight = 'bold';
     text.fill = '#ee0000';
     text.setShadow(0, 0, 'rgba(200, 200, 200, 0.5)', 0);
-
-/*
-    game.add.text(100, 150, 'Entity 1: '+ entityCollection[0][0], { font: "15px Arial", fill: "#19de65" });
-    game.add.text(100, 175, 'Entity 2: '+ entityCollection[1], { font: "15px Arial", fill: "#19de65" });
-    game.add.text(100, 200, 'Entity 3: '+ entityCollection[2].xLoc.toString(), { font: "15px Arial", fill: "#19de65" });
-    game.add.text(100, 300, 'Press up to test:', { font: "15px Arial", fill: "#19de65" });
-*/
 }
 
 function update()
 {
 }
 
-function render()
+function render()   //REMINDER: YOU ARE IN THE ~~RENDER~~ SECTION//
 {
     if (upKey.isDown == true)
     {
         game.add.text(100, 325, 'UP HAS BEEN PRESSED', { font: "15px Arial", fill: "#19de65" });
 
-        //Looks like we can use text object data in variables:
-        game.add.text(entityCollection)
 
-        //Let's try using entity-based object data:
+        //Here we can change the value to an object kept in a global array:
+        entityCollection[2].xLoc=42;
+        game.add.text(100, 350, 'Changes to object: '+ entityCollection[2].xLoc.toString(), { font: "15px Arial", fill: "#19de65" });
 
     }
 }
