af6ac964f3afb1fae9f02d429a8651629d532608
gapspaceengineers <gapspace@engineer.com>
2017-02-26 00:41:39 +0100

    Our Magic Entity Value created! Game variables are now stored in a variable and adapts a bit of OOP. Little Monster Debugger!

--- newGame.js
+++ newGame.js
@@ -8,23 +8,22 @@ var game = new Phaser.Game(1200, 600, Phaser.AUTO, 'phaser-example', {
 var map1 = 'src/map/map1.json';
 var map2 = 'src/map/map2.json';
 
+var mapData;
+var upKey;
+
 function preload() {
     game.load.spritesheet('tiles', 'src/sprites/tiles.png');
     game.load.tilemap('mapData', map1, null, Phaser.Tilemap.TILED_JSON);
     game.load.json('version', map1);
 }
 
-var mapData;
-var upKey;
-
 // Designed to hold a collection of interchangable game objects (i.e player, enemies,etc).
-var entityCollection =[['hello number 1.1','hello number 1.2'],['Hello number 2']];
+var entityCollection = [[],[]];
 
 //Desinged to collect screen text
-var textCollection=[];
+var textCollection = [[],[]];
 
-function create()
-{
+function create(){
     game.physics.startSystem(Phaser.Physics.P2JS);
     upKey = game.input.keyboard.addKey(Phaser.Keyboard.UP);
     runMapData();
@@ -37,17 +36,18 @@ function create()
     }
     //Construct Entity
     var entity1 = new Entity('Player_Entity',10,10);
-    entityCollection.push(entity1)
+    entityCollection.push(entity1);
 
-    //Screen Text
+    //Add Screen Text Here
     textCollection.push('GAME STATS\n');
-    textCollection.push('(ง︡’-‘︠)ง  Entity: ' + entity1.id +' : X: ' + entityCollection[2].xLoc+' Y:'+entityCollection[2].yLoc+'');
+    textCollection.push('(ง︡’-‘︠)ง   Entity From Collection: ' + entityCollection[2].id +' : X: ' + entityCollection[2].xLoc+' Y:'+entityCollection[2].yLoc+'\n');
+    textCollection.push('(ง︡’-‘︠)ง  '+' \n');
+    textCollection.push('(ง︡’-‘︠)ง  '+''+' \n');
+
+    //Text Settings
     var text = game.add.text(10, 10, textCollection);
     text.font = 'Arial Black';
     text.fontSize = 14;
-    //text.fontWeight = 'bold';
-
-    //	Stroke color and thickness
     text.stroke = '#0000ff';
     text.strokeThickness = 1;
     text.fill = '#ffffff';
@@ -62,12 +62,15 @@ function render()   //REMINDER: YOU ARE IN THE ~~RENDER~~ SECTION//
 {
     if (upKey.isDown == true)
     {
-        game.add.text(100, 325, 'UP HAS BEEN PRESSED', { font: "15px Arial", fill: "#19de65" });
+        game.add.text(300, 325, 'UPDATE:', { font: "15px Arial", fill: "#000000" });
 
-
-        //Here we can change the value to an object kept in a global array:
+        //change the value to an object kept in a global array:
         entityCollection[2].xLoc=42;
-        game.add.text(100, 350, 'Changes to object: '+ entityCollection[2].xLoc.toString(), { font: "15px Arial", fill: "#19de65" });
+        game.add.text(300, 350, 'Changes to object: \n'+entityCollection[2].id+'.xloc = '+ entityCollection[2].xLoc, { font: "15px Arial", fill: "#000000" });
+
+        // Display EC list
+        game.add.text(300, 450, 'Items in EntityCollection: '+ entityCollection.length, { font: "15px Arial", fill: "#000000" });
+
 
     }
 }
