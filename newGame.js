1018e8705e4009d420af61001c45eda84a395694
gapspaceengineers <gapspace@engineer.com>
2017-02-26 02:08:22 +0100

    Moved to a efficent debugger. Array based Objects.

--- newGame.js
+++ newGame.js
@@ -9,7 +9,7 @@ var map1 = 'src/map/map1.json';
 var map2 = 'src/map/map2.json';
 
 var mapData;
-var upKey;
+var player;
 
 function preload() {
     game.load.spritesheet('tiles', 'src/sprites/tiles.png');
@@ -21,12 +21,14 @@ function preload() {
 var entityCollection = [[],[]];
 
 //Desinged to collect screen text
-var textCollection = [[],[]];
+var textCollection = [];
 
 function create(){
     game.physics.startSystem(Phaser.Physics.P2JS);
-    upKey = game.input.keyboard.addKey(Phaser.Keyboard.UP);
     runMapData();
+
+    //Create player object from /Player.js
+    player = new Player(300,300);
 
     //Entity Factory
     function Entity(id,xLoc,yLoc){
@@ -35,42 +37,36 @@ function create(){
         this.yLoc = yLoc;
     }
     //Construct Entity
-    var entity1 = new Entity('Player_Entity',10,10);
+    var entity1 = new Entity('Enemy_1',10,10);
     entityCollection.push(entity1);
 
     //Add Screen Text Here
-    textCollection.push('GAME STATS\n');
+        textCollection.push('GAME STATS');
     textCollection.push('(ง︡’-‘︠)ง   Entity From Collection: ' + entityCollection[2].id +' : X: ' + entityCollection[2].xLoc+' Y:'+entityCollection[2].yLoc+'\n');
-    textCollection.push('(ง︡’-‘︠)ง  '+' \n');
-    textCollection.push('(ง︡’-‘︠)ง  '+''+' \n');
 
     //Text Settings
-    var text = game.add.text(10, 10, textCollection);
-    text.font = 'Arial Black';
-    text.fontSize = 14;
-    text.stroke = '#0000ff';
-    text.strokeThickness = 1;
-    text.fill = '#ffffff';
 
 }
 
 function update()
 {
+    //Set up Debug text area
+    var rect = new Phaser.Rectangle( 0, 0,250,250) ;
+    game.debug.geom( rect, 'rgba(0,0,0,0.4)');
+    game.debug.text('Player X:'+player.xLoc+' Y:'+player.yLoc,10,30,"#ffffff");
+    game.debug.text('');
 }
 
 function render()   //REMINDER: YOU ARE IN THE ~~RENDER~~ SECTION//
 {
-    if (upKey.isDown == true)
+    if (game.input.keyboard.addKey(Phaser.Keyboard.UP).isDown == true)
     {
-        game.add.text(300, 325, 'UPDATE:', { font: "15px Arial", fill: "#000000" });
-
-        //change the value to an object kept in a global array:
-        entityCollection[2].xLoc=42;
-        game.add.text(300, 350, 'Changes to object: \n'+entityCollection[2].id+'.xloc = '+ entityCollection[2].xLoc, { font: "15px Arial", fill: "#000000" });
-
-        // Display EC list
-        game.add.text(300, 450, 'Items in EntityCollection: '+ entityCollection.length, { font: "15px Arial", fill: "#000000" });
-
-
+        player.xLoc+=5;
+        player.yLoc+=5;
+    }
+    if (game.input.keyboard.addKey(Phaser.Keyboard.DOWN).isDown == true)
+    {
+        player.xLoc-=5;
+        player.yLoc-=5;
     }
 }
